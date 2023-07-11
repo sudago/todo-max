cc77bf624231352119262442d584cf751d055723
ᴡᴏɴᴋʏᴜɴɢ ᴍɪɴ <mwk0725@gmail.com>
2023-07-12 02:28:06 +0900

    [FE] design: 큰 레이아웃 UI 작업 (#29) (#31)

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -15,15 +15,29 @@ function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Title>test</Title>
-      <Dummy />
-      <DummyTwo />
+      <HeaderLayout>헤더영역</HeaderLayout>
+      <MainLayout>메인영역</MainLayout>
     </ThemeProvider>
   );
 }
 
-const Title = styled.h1`
-  color: ${(props) => props.theme.colors.surfaceBrand};
+const HeaderLayout = styled.div`
+  display: flex;
+  height: 64px;
+  padding: 18px 80px 17px 80px;
+  justify-content: space-between;
+  align-items: center;
+  background-color: ${(props) => props.theme.colors.surfaceAlt};
 `;
+
+const MainLayout = styled.div`
+  padding: 32px 80px 0;
+  height: 960px;
+  background-color: ${(props) => props.theme.colors.surfaceAlt};
+`;
+
+// const Title = styled.h1`
+//   color: ${(props) => props.theme.colors.surfaceBrand};
+// `;
 
 export default App;
--- fe/src/styles/GlobalStyles.ts
+++ fe/src/styles/GlobalStyles.ts
@@ -93,8 +93,16 @@ const GlobalStyles = createGlobalStyle`
   }
 
   body {
-    font-family: 'Pretendard Variable', Pretendard;
+    font-family: "Pretendard Variable", Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
   }
+
+  html {
+    width: 1440px;
+    margin: 0 auto;
+    position: relative;
+    top: 50%;
+    transform: translateY(-50%);
+  };
 `;
 
 export default GlobalStyles;
