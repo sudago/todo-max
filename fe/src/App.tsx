811c47a0e712e587f91e7616f1004d29eb7c2ba4
JH <vvaa277@gmail.com>
2023-07-11 17:04:47 +0900

    [FE] fix: 기본 환경 세팅 오류 수정 (#1)

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,11 +1,10 @@
-import './App.css';
 import { useEffect } from 'react';
 import styled, { ThemeProvider } from 'styled-components';
 import { theme } from './styles/Theme';
 
 function App() {
   useEffect(() => {
-    fetch(`http://52.79.68.54:8080/categories`)
+    fetch('http://52.79.68.54:8080/categories')
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
