e330aeedcba5264b5e320dea5bf4f8b9c8835327
Sully <2dcoder@naver.com>
2023-07-12 17:54:33 +0900

    [BE] style: 우테코 스타일로 코드 정렬

--- be/travelers/src/main/java/codesquad/todolist/travelers/global/ApiResponse.java
+++ be/travelers/src/main/java/codesquad/todolist/travelers/global/ApiResponse.java
@@ -2,23 +2,23 @@ package codesquad.todolist.travelers.global;
 
 public class ApiResponse<T> {
 
-	private String status;
-	private T message;
+    private String status;
+    private T message;
 
-	public ApiResponse(String status, T message) {
-		this.status = status;
-		this.message = message;
-	}
+    public ApiResponse(String status, T message) {
+        this.status = status;
+        this.message = message;
+    }
 
-	public String getStatus() {
-		return status;
-	}
+    public String getStatus() {
+        return status;
+    }
 
-	public T getMessage() {
-		return message;
-	}
+    public T getMessage() {
+        return message;
+    }
 
-	public static <T> ApiResponse<T> success(String status, T message) {
-		return new ApiResponse<>(status, message);
-	}
+    public static <T> ApiResponse<T> success(String status, T message) {
+        return new ApiResponse<>(status, message);
+    }
 }
