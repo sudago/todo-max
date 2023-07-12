f217a6172a5ed35cf8685f767f1ed4b3d688353c
Sully <2dcoder@naver.com>
2023-07-12 17:54:33 +0900

    [BE] style: 우테코 스타일로 코드 정렬

--- be/travelers/src/main/java/codesquad/todolist/travelers/TravelersApplication.java
+++ be/travelers/src/main/java/codesquad/todolist/travelers/TravelersApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TravelersApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TravelersApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TravelersApplication.class, args);
+    }
 
 }
--- be/travelers/src/main/java/codesquad/todolist/travelers/config/SwaggerConfig.java
+++ be/travelers/src/main/java/codesquad/todolist/travelers/config/SwaggerConfig.java
@@ -13,22 +13,22 @@ import springfox.documentation.spring.web.plugins.Docket;
 @Configuration
 public class SwaggerConfig {
 
-	@Bean
-	public Docket api() {
-		return new Docket(DocumentationType.OAS_30)
-			.useDefaultResponseMessages(false)
-			.select()
-			.apis(RequestHandlerSelectors.basePackage("codesquad.todolist.travelers"))
-			.paths(PathSelectors.any())
-			.build()
-			.apiInfo(apiInfo());
-	}
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("codesquad.todolist.travelers"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
 
-	private ApiInfo apiInfo() {
-		return new ApiInfoBuilder()
-			.title("todoList")
-			.description("todoList")
-			.version("1.0")
-			.build();
-	}
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("todoList")
+                .description("todoList")
+                .version("1.0")
+                .build();
+    }
 }
