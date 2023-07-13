e82c421621c022af149b6e00f33da5099115b47d
Sully <2dcoder@naver.com>
2023-07-13 15:25:15 +0900

    [BE] feat: 카드 등록, 조회를 위한 컨트롤러 생성 (#9) (#10)
    - StatusCode 구현 전이기에 임시적으로 ok().body()로 실행만 해봄

---
+++ be/travelers/src/main/java/codesquad/todolist/travelers/task/controller/TaskController.java
@@ -0,0 +1,43 @@
+package codesquad.todolist.travelers.task.controller;
+
+import codesquad.todolist.travelers.global.ApiResponse;
+import codesquad.todolist.travelers.task.domain.dto.ProcessResponseDto;
+import codesquad.todolist.travelers.task.domain.dto.ResponseDto;
+import codesquad.todolist.travelers.task.domain.dto.TaskRequestDto;
+import codesquad.todolist.travelers.task.service.TaskService;
+import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TaskController {
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    // TODO: StatusCode 생성
+    @GetMapping("/")
+    public ResponseEntity<List<ProcessResponseDto>> get() {
+        List<ProcessResponseDto> processes = taskService.getProcesses();
+
+        return ResponseEntity.ok().body(processes);
+//        return ResponseEntity.status(HttpStatus.CREATED)
+//                .body(ApiResponse.success(StatusCode.ORDER_SUCCESS.getCustomStatus(), processes));
+    }
+
+    // TODO: StatusCode 생성
+    @PostMapping("/task")
+    public ResponseEntity<Long> add(@RequestBody final TaskRequestDto taskRequestDto) {
+        Long taskId = taskService.createTask(taskRequestDto);
+
+        return ResponseEntity.ok().body(taskId);
+//        return ResponseEntity.status(HttpStatus.CREATED)
+//                .body(ApiResponse.success(StatusCode.ORDER_SUCCESS.getCustomStatus(), taskId));
+    }
+}
