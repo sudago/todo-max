f26bb73bc14ff5153e7404393fdca29c5b58b9a9
Sully <2dcoder@naver.com>
2023-07-13 16:48:57 +0900

    [BE] refactor: Response 반환 타입 변경 (#11)
    - 커스텀 ResposeDto 반환했던 것에서 공통적으로 만들었던 ApiResponse 활용

--- be/travelers/src/main/java/codesquad/todolist/travelers/task/controller/TaskController.java
+++ be/travelers/src/main/java/codesquad/todolist/travelers/task/controller/TaskController.java
@@ -1,14 +1,15 @@
 package codesquad.todolist.travelers.task.controller;
 
 import codesquad.todolist.travelers.global.ApiResponse;
-import codesquad.todolist.travelers.task.domain.dto.ProcessResponseDto;
-import codesquad.todolist.travelers.task.domain.dto.ResponseDto;
-import codesquad.todolist.travelers.task.domain.dto.TaskRequestDto;
+import codesquad.todolist.travelers.task.domain.dto.response.ProcessResponseDto;
+import codesquad.todolist.travelers.task.domain.dto.request.TaskRequestDto;
 import codesquad.todolist.travelers.task.service.TaskService;
 import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,24 +22,20 @@ public class TaskController {
         this.taskService = taskService;
     }
 
-    // TODO: StatusCode 생성
     @GetMapping("/")
-    public ResponseEntity<List<ProcessResponseDto>> get() {
+    public ResponseEntity<ApiResponse<?>> get() {
         List<ProcessResponseDto> processes = taskService.getProcesses();
 
-        return ResponseEntity.ok().body(processes);
-//        return ResponseEntity.status(HttpStatus.CREATED)
-//                .body(ApiResponse.success(StatusCode.ORDER_SUCCESS.getCustomStatus(), processes));
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(ApiResponse.success("200", processes));
     }
 
-    // TODO: StatusCode 생성
     @PostMapping("/task")
-    public ResponseEntity<Long> add(@RequestBody final TaskRequestDto taskRequestDto) {
+    public ResponseEntity<ApiResponse<?>> add(@RequestBody final TaskRequestDto taskRequestDto) {
         Long taskId = taskService.createTask(taskRequestDto);
 
-        return ResponseEntity.ok().body(taskId);
-//        return ResponseEntity.status(HttpStatus.CREATED)
-//                .body(ApiResponse.success(StatusCode.ORDER_SUCCESS.getCustomStatus(), taskId));
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(ApiResponse.success("200", taskId));
     }
 
     @DeleteMapping("/task/{taskId}")
