172afa19333b9134d5bbc67403749faa0e1d8a1c
Sully <2dcoder@naver.com>
2023-07-13 17:52:26 +0900

    [BE] feat: 컨트롤러에 필요한 DTO 구현 (#13)
    - Long 타입으로 하려면 RequestBody에서 중괄호를 빼야 함

--- be/travelers/src/main/java/codesquad/todolist/travelers/task/domain/dto/response/TaskResponseDto.java
+++ be/travelers/src/main/java/codesquad/todolist/travelers/task/domain/dto/response/TaskResponseDto.java
@@ -8,16 +8,12 @@ public class TaskResponseDto {
     private final String title;
     private final String contents;
     private final String platform;
-    private final LocalDateTime createdTime;
-    private final Long processId;
 
     public TaskResponseDto(final Task task) {
         this.taskId = task.getTaskId();
         this.title = task.getTitle();
         this.contents = task.getContents();
         this.platform = task.getPlatform();
-        this.createdTime = task.getCreatedTime();
-        this.processId = task.getProcessId();
     }
 
     public Long getTaskId() {
@@ -34,13 +30,5 @@ public class TaskResponseDto {
 
     public String getPlatform() {
         return platform;
-    }
-
-    public LocalDateTime getCreatedTime() {
-        return createdTime;
-    }
-
-    public Long getProcessId() {
-        return processId;
     }
 }
