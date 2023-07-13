f5918243159f8983b08cea6ce05dcb817907c4f0
CDBchan <kdjin1215@naver.com>
2023-07-13 22:19:26 +0900

    [BE] feat: 활동기록 조회 API 구현 (#18)

--- be/travelers/src/main/java/codesquad/todolist/travelers/history/domain/repository/HistoryRepository.java
+++ be/travelers/src/main/java/codesquad/todolist/travelers/history/domain/repository/HistoryRepository.java
@@ -1,5 +1,10 @@
 package codesquad.todolist.travelers.history.domain.repository;
 
+import codesquad.todolist.travelers.history.domain.ActionHistory;
+import java.util.List;
+
 public interface HistoryRepository {
-    public void deleteAll();
+    void deleteAll();
+
+    List<ActionHistory> findAll();
 }
--- be/travelers/src/main/java/codesquad/todolist/travelers/history/domain/repository/JdbcHistoryRepositoryImpl.java
+++ be/travelers/src/main/java/codesquad/todolist/travelers/history/domain/repository/JdbcHistoryRepositoryImpl.java
@@ -1,20 +1,53 @@
 package codesquad.todolist.travelers.history.domain.repository;
 
+import codesquad.todolist.travelers.history.domain.ActionHistory;
+import java.util.List;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class JdbcHistoryRepositoryImpl implements HistoryRepository {
-    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+    private final NamedParameterJdbcTemplate template;
 
-    public JdbcHistoryRepositoryImpl(NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
-        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
+    public JdbcHistoryRepositoryImpl(NamedParameterJdbcTemplate template) {
+        this.template = template;
     }
 
     @Override
     public void deleteAll() {
         String sql = "DELETE FROM history";
 
-        namedParameterJdbcTemplate.getJdbcTemplate().update(sql);
+        template.getJdbcTemplate().update(sql);
     }
+
+    @Override
+    public List<ActionHistory> findAll() {
+        String sql =
+                "SELECT h.title, h.from, h.to, h.created_time, a.name AS action_name, u.name AS user_name, u.image_url "
+                        + "FROM history h "
+                        + "JOIN action a ON a.action_id = h.action_id "
+                        + "JOIN user u ON u.user_id = h.user_id "
+                        + "ORDER BY h.created_time DESC ";
+
+        return template.query(sql, ActionhistoryRowMapper());
+    }
+
+    /**
+     * 첫번째 name: action 테이블의 name, 두번째 name: user 테이블의 name
+     *
+     * @return
+     */
+    private RowMapper<ActionHistory> ActionhistoryRowMapper() {
+        return ((rs, rowNum) -> new ActionHistory(
+                rs.getString("title"),
+                rs.getString("from"),
+                rs.getString("to"),
+                rs.getString("action_name"),
+                rs.getTimestamp("created_time").toLocalDateTime(),
+                rs.getString("user_name"),
+                rs.getString("image_url")
+        ));
+    }
+
 }
