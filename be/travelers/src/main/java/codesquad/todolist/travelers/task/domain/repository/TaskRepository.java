df19bd4b2cef5bfd43f657c8eb3f40598dd0bc5b
Sully <2dcoder@naver.com>
2023-07-13 15:23:36 +0900

    [BE] feat: 카드 등록, 조회를 위한 레포지토리 생성 (#9) (#10)

---
+++ be/travelers/src/main/java/codesquad/todolist/travelers/task/domain/repository/TaskRepository.java
@@ -0,0 +1,13 @@
+package codesquad.todolist.travelers.task.domain.repository;
+
+import codesquad.todolist.travelers.task.domain.entity.Process;
+import codesquad.todolist.travelers.task.domain.entity.Task;
+import java.util.List;
+
+public interface TaskRepository {
+    Long save(final Task task);
+
+    List<Task> findAllBy(final Long processId);
+
+    List<Process> findProcesses();
+}
