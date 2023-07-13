8b22c6cdc2f88cb7cb33e1b9b43721a9259eadc7
Sully <2dcoder@naver.com>
2023-07-13 15:24:38 +0900

    [BE] feat: 카드 등록, 조회를 위한 서비스 생성 (#9) (#10)

---
+++ be/travelers/src/main/java/codesquad/todolist/travelers/task/service/TaskService.java
@@ -0,0 +1,45 @@
+package codesquad.todolist.travelers.task.service;
+
+import codesquad.todolist.travelers.task.domain.dto.ProcessResponseDto;
+import codesquad.todolist.travelers.task.domain.dto.TaskRequestDto;
+import codesquad.todolist.travelers.task.domain.dto.TaskResponseDto;
+import codesquad.todolist.travelers.task.domain.entity.Process;
+import codesquad.todolist.travelers.task.domain.repository.TaskRepository;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@Service
+public class TaskService {
+    private final TaskRepository taskRepository;
+
+    public TaskService(final TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public Long createTask(final TaskRequestDto taskRequestDto) {
+        return taskRepository.save(taskRequestDto.toEntity());
+    }
+
+    public List<ProcessResponseDto> getProcesses() {
+        List<Process> processes = taskRepository.findProcesses();
+
+        List<ProcessResponseDto> processResponseDtoList = new ArrayList<>();
+        for (Process process : processes) {
+            ProcessResponseDto from = ProcessResponseDto.from(process, getTasksBy(process.getProcessId()));
+            processResponseDtoList.add(from);
+        }
+
+        return processResponseDtoList;
+    }
+
+    private List<TaskResponseDto> getTasksBy(final Long processId) {
+        return taskRepository.findAllBy(processId)
+                .stream()
+                .map(TaskResponseDto::new)
+                .collect(Collectors.toUnmodifiableList());
+    }
+}
