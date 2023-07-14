8e75a2bb575943d3bc17f27c8a41d4689c675302
Sully <2dcoder@naver.com>
2023-07-14 12:08:22 +0900

    [BE] feat: TaskController에 Swagger 적용 (#60)

--- be/travelers/src/main/java/codesquad/todolist/travelers/task/controller/TaskController.java
+++ be/travelers/src/main/java/codesquad/todolist/travelers/task/controller/TaskController.java
@@ -7,6 +7,8 @@ import codesquad.todolist.travelers.task.domain.dto.request.TaskUpdateRequestDto
 import codesquad.todolist.travelers.task.domain.dto.response.ProcessResponseDto;
 import codesquad.todolist.travelers.task.domain.dto.response.TaskPostResponseDto;
 import codesquad.todolist.travelers.task.service.TaskService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,6 +28,7 @@ public class TaskController {
         this.taskService = taskService;
     }
 
+    @Operation(summary = "todolist 가져오기", description = "서버로부터 todolist에 필요한 모든 데이터를 가져온다.")
     @GetMapping("/")
     public ResponseEntity<ApiResponse<?>> get() {
         List<ProcessResponseDto> processes = taskService.getProcesses();
@@ -34,6 +37,7 @@ public class TaskController {
                 .body(ApiResponse.success("200", processes));
     }
 
+    @Operation(summary = "카드 등록", description = "POST 요청으로 각 칼럼(process)에 대한 task를 등록한다.")
     @PostMapping("/task")
     public ResponseEntity<ApiResponse<?>> add(@RequestBody final TaskRequestDto taskRequestDto) {
         TaskPostResponseDto task = taskService.createTask(taskRequestDto);
@@ -42,6 +46,8 @@ public class TaskController {
                 .body(ApiResponse.success("200", task));
     }
 
+    @Operation(summary = "카드 삭제", description = "DELETE 요청으로 고유 ID에 따른 카드를 삭제한다.")
+    @Parameter(name = "taskId", description = "카드의 고유 ID")
     @DeleteMapping("/task/{taskId}")
     public ResponseEntity<ApiResponse<?>> delete(@PathVariable final Long taskId) {
         taskService.deleteTask(taskId);
@@ -50,6 +56,8 @@ public class TaskController {
                 .body(ApiResponse.success("200", "카드 삭제 성공!"));
     }
 
+    @Operation(summary = "카드 수정", description = "PATCH 요청으로 고유 ID에 따른 카드를 수정한다.")
+    @Parameter(name = "taskId", description = "카드의 고유 ID")
     @PatchMapping("/task/{taskId}")
     public ResponseEntity<ApiResponse<?>> update(@PathVariable final Long taskId,
                                                  @RequestBody final TaskUpdateRequestDto taskUpdateRequestDto) {
@@ -59,6 +67,8 @@ public class TaskController {
                 .body(ApiResponse.success("200", "카드 수정 성공!"));
     }
 
+    @Operation(summary = "카드 이동", description = "PATCH 요청으로 원하는 칼럼(process)으로 카드를 이동시킨다. body로는 processId를 받는다.")
+    @Parameter(name = "taskId", description = "카드의 고유 ID")
     @PatchMapping("/task/process/{taskId}")
     public ResponseEntity<ApiResponse<?>> move(@PathVariable final Long taskId,
                                                @RequestBody final TaskProcessIdRequestDto taskProcessIdRequestDto) {
