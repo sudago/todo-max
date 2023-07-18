fc1630039dfb4386da62cb3b0182022a84aee061
CDBchan <kdjin1215@naver.com>
2023-07-18 18:09:07 +0900

    [BE] test: 단위 테스트 코드 작성 (#73)
    history의 service, task의 repository를 제외한 단위테스트코드를 작성했습니다.

--- be/travelers/src/test/java/codesquad/todolist/travelers/annotation/RepositoryTest.java
+++ be/travelers/src/test/java/codesquad/todolist/travelers/annotation/RepositoryTest.java
@@ -6,10 +6,12 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @SpringBootTest
 @ActiveProfiles("test")
+@Sql(scripts = {"classpath:schema.sql"})
 public @interface RepositoryTest {
 }
